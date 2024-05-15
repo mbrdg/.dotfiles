c534a5d275f9d7c1efda30230881414509ee5216
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-05-15 01:17:09 +0100

    Simplify nvim config with mini plugins

--- nvim/.config/nvim/lua/mbrdg/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
--- plugins/comment.lua
--- easy comment toogler
-
-return {
-  'numToStr/Comment.nvim',
-  opts = {},
-}
---
+++ nvim/.config/nvim/lua/mbrdg/plugins/mini.lua
@@ -0,0 +1,26 @@
+-- plugins/mini.ai
+-- collection of several small independent plugins
+
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    require('mini.ai').setup()
+    require('mini.comment').setup()
+    local hipatterns = require 'mini.hipatterns'
+    hipatterns.setup {
+      highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        -- fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+        -- hack = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+        -- todo = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+        -- note = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
+
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        -- hex_color = hipatterns.gen_highlighter.hex_color(),
+      },
+    }
+    require('mini.surround').setup()
+    require('mini.pairs').setup()
+    require('mini.trailspace').setup()
+  end,
+}
