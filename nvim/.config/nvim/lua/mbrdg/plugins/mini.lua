e58f12dac819482b6a23b7be10898dcc4f31ec8d
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-05-26 02:15:44 +0100

    Updated nvim config: simplified statusline

--- nvim/.config/nvim/lua/mbrdg/plugins/mini.lua
+++ nvim/.config/nvim/lua/mbrdg/plugins/mini.lua
@@ -5,6 +5,15 @@ return {
   'echasnovski/mini.nvim',
   config = function()
     require('mini.ai').setup()
+
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = false }
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
+
     require('mini.surround').setup()
     require('mini.pairs').setup()
     require('mini.trailspace').setup()
