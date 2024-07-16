6d4e73a2920a2c2fae4a840a6daec5ad1133053e
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-07-16 15:06:08 +0100

    Updated nvim config: fixed deprecations

--- nvim/.config/nvim/lua/mbrdg/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mbrdg/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
   },
   opts = {
-    ensure_installed = { 'c', 'cpp', 'rust', 'zig', 'python', 'lua', 'make', 'markdown', 'diff' },
+    ensure_installed = { 'c', 'cpp', 'rust', 'zig', 'python', 'lua', 'make', 'diff' },
     auto_install = false,
     highlight = { enable = true },
     indent = { enable = true },
