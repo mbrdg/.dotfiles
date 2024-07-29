80817051ddcbb75b28e8db60403665b9e5cb7cad
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-07-29 01:09:51 +0100

    Update nvim config: remove treesitter prefer_git option

--- nvim/.config/nvim/lua/mbrdg/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/mbrdg/plugins/treesitter.lua
@@ -83,7 +83,6 @@ return {
     },
   },
   config = function(_, opts)
-    require('nvim-treesitter.install').prefer_git = true
     require('nvim-treesitter.configs').setup(opts)
     require('treesitter-context').setup {
       enable = false,
