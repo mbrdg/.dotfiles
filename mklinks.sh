99250cabaf361cad34bc58c374fb3433c2961872
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-08-21 03:23:07 +0100

    Restructure: fix missing zsh links

--- mklinks.sh
+++ mklinks.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 mkdir -p ~/.config
+ln -sfn ~/.dotfiles/zsh/.zshenv ~/.zshenv
+ln -sfn ~/.dotfiles/zsh/.config ~/.config/zsh
 ln -sfn ~/.dotfiles/git ~/.config/git
 ln -sfn ~/.dotfiles/tmux ~/.config/tmux
 ln -sfn ~/.dotfiles/nvim ~/.config/nvim
