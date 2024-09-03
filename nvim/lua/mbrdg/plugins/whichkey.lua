f7240c80ac6a315fac23af7e8fb734728205a830
Miguel Rodrigues <migb.rodrigues@gmail.com>
2024-09-03 21:50:11 +0100

    Update nvim config: which-key config fix

--- nvim/lua/mbrdg/plugins/whichkey.lua
+++ nvim/lua/mbrdg/plugins/whichkey.lua
@@ -4,16 +4,16 @@
 return {
   'folke/which-key.nvim',
   event = 'VimEnter',
-  spec = {
-    { '<leader>c', group = '[C]ode' },
-    { '<leader>d', group = '[D]ocument' },
-    { '<leader>r', group = '[R]ename' },
-    { '<leader>s', group = '[S]earch' },
-    { '<leader>w', group = '[W]orkspace' },
-    { '<leader>t', group = '[T]oggle' },
-    { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-  },
   opts = {
+    spec = {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+    },
     icons = {
       mappings = vim.g.have_nerd_font,
     },
