7c03cedc9c9379c9393c01be66d46980f774b373
Miguel Rodrigues <migb.rodrigues@gmail.com>
2025-01-26 15:39:41 +0000

    Update nvim config: changed preview width for telescope's layout

--- nvim/lua/mbrdg/plugins/telescope.lua
+++ nvim/lua/mbrdg/plugins/telescope.lua
@@ -2,52 +2,65 @@
 -- file and symbol finder
 
 return {
-  'nvim-telescope/telescope.nvim',
-  event = 'VimEnter',
-  branch = '0.1.x',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    -- fuzzy finder algorithm that requires local deps to be built
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    'nvim-telescope/telescope-ui-select.nvim',
-  },
-  config = function()
-    require('telescope').setup {
-      extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
-        },
-      },
-    }
+	'nvim-telescope/telescope.nvim',
+	event = 'VimEnter',
+	branch = '0.1.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		-- fuzzy finder algorithm that requires local deps to be built
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+		'nvim-telescope/telescope-ui-select.nvim',
+		'nvim-telescope/telescope-file-browser.nvim',
+	},
+	config = function()
+		require('telescope').setup {
+			defaults = {
+				layout_strategy = 'horizontal',
+				layout_config = {
+					horizontal = { width = 0.9, preview_width = 0.5 },
+				},
+			},
+			extensions = {
+				['ui-select'] = {
+					require('telescope.themes').get_dropdown(),
+				},
+			},
+		}
 
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
+		pcall(require('telescope').load_extension, 'fzf')
+		pcall(require('telescope').load_extension, 'ui-select')
+		pcall(require('telescope').load_extension, 'file_browser')
 
-    local builtin = require 'telescope.builtin'
-    local km = vim.keymap
-    km.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    km.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    km.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    km.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    km.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    km.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    km.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    km.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    km.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    km.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+		local builtin = require 'telescope.builtin'
+		local km = vim.keymap
+		km.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+		km.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+		km.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+		km.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+		km.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+		km.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+		km.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+		km.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+		km.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		km.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
-    km.set('n', '<leader>/', function()
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-        winblend = 10,
-        previewer = false,
-      })
-    end, { desc = '[/] Fuzzily search in current buffer' })
+		km.set('n', '<leader>/', function()
+			builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+				winblend = 10,
+				previewer = false,
+			})
+		end, { desc = '[/] Fuzzily search in current buffer' })
 
-    km.set('n', '<leader>s/', function()
-      builtin.live_grep {
-        grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
-      }
-    end, { desc = '[S]earch [/] in Open Files' })
-  end,
+		km.set('n', '<leader>s/', function()
+			builtin.live_grep {
+				grep_open_files = true,
+				prompt_title = 'Live Grep in Open Files',
+			}
+		end, { desc = '[S]earch [/] in Open Files' })
+
+		-- file browser from telescope
+		km.set('n', '<leader>fb', function()
+			require('telescope').extensions.file_browser.file_browser()
+		end, { desc = 'Open [F]ile [B]rowser' })
+	end,
 }
