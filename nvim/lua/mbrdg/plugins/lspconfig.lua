095ca147c9cf86e5937e1bddd7f3b9ad3d38c439
Miguel Rodrigues <migb.rodrigues@gmail.com>
2025-05-23 12:32:25 +0100

    Update nvim config: add gopls config

--- nvim/lua/mbrdg/plugins/lspconfig.lua
+++ nvim/lua/mbrdg/plugins/lspconfig.lua
@@ -102,7 +102,22 @@ return {
 					},
 				},
 			},
-			gopls = {},
+			gopls = {
+				settings = {
+					gopls = {
+						gofumpt = true,
+						hints = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							compositeLiteralTypes = true,
+							constantValues = true,
+							functionTypeParameters = true,
+							parameterNames = true,
+							rangeVariableTypes = true,
+						}
+					}
+				}
+			},
 			pylsp = {},
 			rust_analyzer = {},
 			zls = {},
