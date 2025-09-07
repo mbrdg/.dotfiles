4886ef4678fb618c05057287c536e6d5387f4713
Miguel Rodrigues <migb.rodrigues@gmail.com>
2025-09-07 23:21:10 +0100

    Reintroduce ghostty config

--- mklinks.sh
+++ mklinks.sh
@@ -4,5 +4,6 @@ mkdir -p ~/.config
 ln -sfn ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -sfn ~/.dotfiles/zsh/.config ~/.config/zsh
 ln -sfn ~/.dotfiles/git ~/.config/git
+ln -sfn ~/.dotfiles/ghostty/ ~/.config/ghostty
 ln -sfn ~/.dotfiles/tmux ~/.config/tmux
 ln -sfn ~/.dotfiles/nvim ~/.config/nvim
