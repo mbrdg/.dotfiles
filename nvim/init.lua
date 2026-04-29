8c79dba3e497b0db863945237328f532596429b8
Miguel Rodrigues <migb.rodrigues@gmail.com>
2026-04-29 23:27:59 +0100

    editor: remove gram & nvim

--- nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- init.lua
--- nvim config entry file
-
-require('mbrdg.core.options')
-require('mbrdg.core.keymaps')
-require('mbrdg.core.autocmds')
-
-require('mbrdg.lazy')
--- nvim/lua/mbrdg/plugins/init.lua
+++
@@ -1,7 +0,0 @@
--- plugins/init.lua
--- plugins that require no configuration
-
-return {
-  -- detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-}
